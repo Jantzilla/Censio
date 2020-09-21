2646766f43aaca20be74f636ca7c3907253eb21c
Jantz <jantzillac.93@gmail.com>
2020-09-21 12:49:03 -0500

    Create inject method for each dependent class

--- app/src/main/java/com/apps/creativesource/censio/AppComponent.java
+++ app/src/main/java/com/apps/creativesource/censio/AppComponent.java
@@ -8,4 +8,10 @@ import dagger.Component;
 @Component(modules={AppModule.class, UtilModule.class})
 public interface AppComponent {
     void inject(LoginActivity activity);
+    void inject(AddActivity activity);
+    void inject(SettingActivity activity);
+    void inject(ChoiceDetailFragment fragment);
+    void inject(CommentDetailFragment fragment);
+    void inject(SettingsFragment fragment);
+    void inject(NotificationService service);
 }
