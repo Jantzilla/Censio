fc91fe1dc8370f05835641f4a15635156dadb751
Jantz <jantzillac.93@gmail.com>
2020-09-21 11:59:28 -0500

    Create Application class for Dagger component use

---
+++ app/src/main/java/com/apps/creativesource/censio/MyApp.java
@@ -0,0 +1,21 @@
+package com.apps.creativesource.censio;
+
+import android.app.Application;
+
+public class MyApp extends Application {
+
+    private AppComponent mAppComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mAppComponent = DaggerAppComponent.builder()
+                .appModule(new AppModule(this))
+                .build();
+    }
+
+    public AppComponent getAppComponent() {
+        return mAppComponent;
+    }
+}
